2681930c834fad81164f9e49ed9eada2dd3c295d
Mohammad Alaei <alaei.py@gmail.com>
2022-04-10 18:05:59 +0430

    03-vidly: complete exercises

---
+++ 03-vidly/src/components/movies.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = { movies: getMovies() };
+
+  render() {
+    const count = this.state.movies.length;
+    if (count === 0)
+      return <p className="pt-5 bold">There are no movies in the database!</p>;
+    return (
+      <React.Fragment>
+        <p className="pt-5 bold">
+          Showing {this.state.movies.length} movies in the database
+        </p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => {
+              return (
+                <tr key={movie._id}>
+                  <th scope="row">{movie.title}</th>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDeleteMovie(movie)}
+                      className="btn btn-danger btn-sm"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+
+  handleDeleteMovie = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+}
+
+export default Movies;
