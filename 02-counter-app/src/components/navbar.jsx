5425c01b090ac2b33a7cf85df0ae2f55edcac56e
Mohammad Alaei <alaei.py@gmail.com>
2022-04-11 18:12:22 +0430

    02-counter-app: destructure arguments.

--- 02-counter-app/src/components/navbar.jsx
+++ 02-counter-app/src/components/navbar.jsx
@@ -2,14 +2,12 @@ import React, { Component } from "react";
 
 //stateless functional component
 
-const NavBar = (props) => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar
-        <span className="badge bg-secondary bg-sm m-2">
-          {props.totalCounters}
-        </span>
+        <span className="badge bg-secondary bg-sm m-2">{totalCounters}</span>
       </a>
     </nav>
   );
