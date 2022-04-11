b994545b4ed2e953198fd8c22342447300800aef
Mohammad Alaei <alaei.py@gmail.com>
2022-04-11 18:06:56 +0430

    02-counter-app:change NavBar class component to stateless class component.

--- 02-counter-app/src/components/navbar.jsx
+++ 02-counter-app/src/components/navbar.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge bg-secondary bg-sm m-2">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+//stateless functional component
+
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge bg-secondary bg-sm m-2">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
