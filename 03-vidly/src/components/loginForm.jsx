01e6403de4b3ae761753c0d57dba4ca154587cda
Mohammad Alaei <alaei.py@gmail.com>
2022-04-15 16:20:53 +0430

    03-vidly: handle form multiple inputs.

--- 03-vidly/src/components/loginForm.jsx
+++ 03-vidly/src/components/loginForm.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 class LoginForm extends Component {
-  username = React.createRef();
+  state = {
+    account: { username: "", password: "" },
+  };
 
-  componentDidMount() {
-    this.username.current.focus();
-  }
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
   handleSubmit = (e) => {
     e.preventDefault();
 
-    const username = this.username.current.value;
     //Call the server
     console.log("submitted");
   };
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -20,15 +24,25 @@ class LoginForm extends Component {
           <div className="form-group py-2">
             <label htmlFor="username">Username</label>
             <input
-              ref={this.username}
+              autoFocus
+              value={account.username}
+              onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group py-2">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary" type="submit">
             Login
