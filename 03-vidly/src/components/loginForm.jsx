bf787af1c6b2369166df7fec7c0f74e92cd3dbfe
Mohammad Alaei <alaei.py@gmail.com>
2022-04-15 15:55:50 +0430

    03-vidly: handle form submission.

--- 03-vidly/src/components/loginForm.jsx
+++ 03-vidly/src/components/loginForm.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from "react";
+class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
 
-const LoginForm = () => {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div className="form-group py-2">
-          <label htmlFor="username">Username</label>
-          <input id="username" type="text" className="form-control" />
-        </div>
-        <div className="form-group py-2">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="text" className="form-control" />
-        </div>
-        <button className="btn btn-primary" type="submit">
-          Login
-        </button>
-      </form>
-    </div>
-  );
-};
+    //Call the server
+    console.log("submitted");
+  };
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group py-2">
+            <label htmlFor="username">Username</label>
+            <input id="username" type="text" className="form-control" />
+          </div>
+          <div className="form-group py-2">
+            <label htmlFor="password">Password</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default LoginForm;
