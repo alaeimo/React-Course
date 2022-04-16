551cd2d91ba582eb107ed02473498a187b9c2caf
Mohammad Alaei <alaei.py@gmail.com>
2022-04-16 16:33:43 +0430

    03-vidle: extract helper rendering methods.

--- 03-vidly/src/components/loginForm.jsx
+++ 03-vidly/src/components/loginForm.jsx
@@ -1,57 +1,28 @@
 import React, { Component } from "react";
-import Input from "./common/input";
-class LoginForm extends Component {
+import Joi from "joi-browser";
+import Form from "./common/from";
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {},
   };
 
-  validate = () => {
-    const errors = {};
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required!";
-    if (account.password.trim() === "")
-      errors.password = "Password is required!";
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
-    return Object.keys(errors).length === 0 ? null : errors;
-  };
-  handleChange = ({ currentTarget: input }) => {
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account });
-  };
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-    //Call the server
+  doSubmit = () => {
     console.log("submitted");
   };
   render() {
-    const { account, errors } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <Input
-            name="username"
-            label="Usename"
-            value={account.username}
-            onChange={this.handleChange}
-            error={errors.username}
-          />
-          <Input
-            name="password"
-            label="pasword"
-            value={account.password}
-            onChange={this.handleChange}
-            error={errors.password}
-          />
-          <button className="btn btn-primary" type="submit">
-            Login
-          </button>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderButton("Login")}
         </form>
       </div>
     );
